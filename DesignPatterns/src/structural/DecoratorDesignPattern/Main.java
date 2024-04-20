f4abcaf4a237bc1872de8fc6b7e514093307e4bd
ishant <ishantkumar1412@gmail.com>
2024-04-20 20:35:01 +0530

    Add description for DecoratorDP

--- DesignPatterns/src/structural/DecoratorDesignPattern/Main.java
+++ DesignPatterns/src/structural/DecoratorDesignPattern/Main.java
@@ -1,5 +1,13 @@
 package structural.DecoratorDesignPattern;
 
+/**
+ * 
+ * @author ishant
+ * this is used to modify the functionality of object at runtime.
+ * In this case, we can add multiple toppings of Pizza with extensible no need to create multiple classes
+ * for each combination.
+ */
+
 public class Main {
 	public static void main(String[] args) {
 		Pizza pizza = new BasePizza();
